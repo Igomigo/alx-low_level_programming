67472a661f05d025704d822e5c0497284359b8d7
Igomigo <igomigofatai@gmail.com>
2022-12-11 02:11:42 -0800

    Small letters of the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all the alphabet in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char letter;
+	
+	for(letter = a; a <= z; letter++)
+		putchar(letter);
+
+	putchar(\n);
+	return = 0;
+}
