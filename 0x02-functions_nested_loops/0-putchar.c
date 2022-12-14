b7760ff0a9ff9dd97e26a618fc574f7055f2bf02
Igomigo <igomigofatai@gmail.com>
2022-12-14 13:22:01 -0800

    Task o

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - Print _putchar
- * Return: Always 0
+ * main - print putchar
+ * Return: always 0
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -15,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
