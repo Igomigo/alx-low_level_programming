0a210b5d2010e9c95d3e8ecf6584b6292ca0eb0d
Igomigo <igomigofatai@gmail.com>
2022-12-24 14:26:41 -0800

    Numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - Functions that print 0 to 9 numbers
- *
- * Return: void
- */
+*print_numbers - print 0123456789
+* owned by Vikcent
+*Return: void
+*/
 
 void print_numbers(void)
 {
-	int num;
+	char c;
 
-	for (num = 0; num <= 9; num++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(num);
+	_putchar(c);
 	}
-
 	_putchar('\n');
 }
