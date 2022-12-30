675ba9eab779edad1ea0e9c2f4e40372a5d2f47f
Igomigo <igomigofatai@gmail.com>
2022-12-30 01:40:59 -0800

    Function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - Function that prints a string to stdout
+ * @str: string variable
+ *
+ * Return: the length of the string
+ */
+
+void _puts(char *str)
+{
+	_putchar(str);
+
+	_putchar("\n");
+}
