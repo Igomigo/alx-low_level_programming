49360446738e23e4c716169e1cc7b980ae2695a7
Igomigo <igomigofatai@gmail.com>
2023-01-06 05:46:00 -0800

    Recursive function that prints a string in reverse

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
-		return;
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_puts_recursion(s + 1);
-	_putchar(*s);
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
